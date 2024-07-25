af3af16cbf0360e88a701be74afbf8f6b5ec4425
gsbad <gsbadbr@gmail.com>
2024-07-24 23:12:09 -0300

    MappingStreams

--- MappingStreams.java
+++ MappingStreams.java
@@ -15,28 +15,30 @@ public class MappingStreams {
         //LongStream       LongFunction<R>   LongToDoubleFunction    LongToIntFunction   LongUnaryOperator
 
         //Mapping methods between type of Streams
-        IntStream intStream = objStream.mapToInt(s->s.length());//Parameter: ToIntFunction
+        IntStream intStream = objStream.mapToInt(String::length);//Parameter: ToIntFunction
         LongStream longStream = intStream.mapToLong(i->i); //Paramether: IntToLongFunction
         Stream<Long> backToLongStream = longStream.mapToObj(l->l); //Parameter: LongFunction<R>
         DoubleStream doubleStream = backToLongStream.mapToDouble(d->d); //Parameter: ToDoubleFunction<T>
-        //etc, etc etc...      
+        //etc, etc etc...
 
         var integerList = new ArrayList<Integer>();
         integerList.add(1);
         integerList.add(2);
         integerList.add(3);
-        
-        IntStream ints = integerList.stream().flatMapToInt(x->IntStream.of(x));
+
+        IntStream ints = integerList.stream().flatMapToInt(IntStream::of); //Method reference
         DoubleStream doubles = integerList.stream().flatMapToDouble(x->DoubleStream.of(x));
-        LongStream longs = integerList.stream().flatMapToLong(x->LongStream.of(x));
+        LongStream longs = integerList.stream().flatMapToLong(LongStream::of); //Method Reference
 
         //Using Optional with primitive stream
         var stream = IntStream.rangeClosed(1, 10);
         OptionalDouble optional = stream.average(); //average() always returns OptionalDouble
 
         optional.ifPresent(System.out::println); //5.5
-        System.out.println(optional.getAsDouble()); //5.5
-        
+        if (optional.isPresent()) {
+            System.out.println(optional.getAsDouble()); //5.5
+        }
+
         long sum = longs.sum();
         System.out.println(sum); //6
         DoubleStream doubles2 = DoubleStream.generate(()->Math.PI);

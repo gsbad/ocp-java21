b804c9ba0e7d08c811efa93b286383e7087f0623
Gustavo Sá <gsbadbr@gmail.com>
2024-08-11 15:58:00 -0300

    Synchronizing on methods

--- ThreadSafe.java
+++ ThreadSafe.java
@@ -1,26 +1,38 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadSafe {
 //    private volatile int sheepCount = 0; Not so used! Can show up on exam!
 //    private AtomicInteger sheepCount = new AtomicInteger(0);
     private int sheepCount = 0;
     //SheepManager
-//    private void incrementAndReport(){
-//        synchronized (this){              //SYNCHRONIZED BLOCK
-//            System.out.print((++sheepCount) + " ");
-//        }
-//    } EQUIVALENT WITH:
-    private synchronized void incrementAndReport(){     //SYNCHRONIZED METHOD MODIFIER
+    private void incrementAndReportBlockSync(){
+        synchronized (this){              //SYNCHRONIZED BLOCK
+            System.out.print((++sheepCount) + " ");
+        }
+    } //EQUIVALENT WITH:
+    private synchronized void incrementAndReportMethodModifier(){     //SYNCHRONIZED METHOD MODIFIER
         System.out.print((++sheepCount) + " ");
+    }
+    private synchronized void incrementAndReportWithLock(){
+        var lock = new ReentrantLock(true);
+        //LOCK: ALSO EQUIVALENT (fair method parameter on 'true' for when ordering is required
+        try {
+            lock.lock();
+            System.out.print((++sheepCount) + " ");
+        } finally {
+            //'IllegalMonitorStateException' if the lock does not exist
+            lock.unlock();
+        }
     }
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(20);
         try {
             var manager = new ThreadSafe();
             for (int i = 0; i < 10; i++)
-                service.submit(manager::incrementAndReport);
+                service.submit(manager::incrementAndReportWithLock);
         }finally{
             service.shutdown();
         }

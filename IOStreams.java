8047a0b64c4161d614b715aa4b8ed054443e7a50
gsbad <gsbadbr@gmail.com>
2024-08-17 20:12:40 -0300

    Enhancing with Files class (NIO2 API)

--- IOStreams.java
+++ IOStreams.java
@@ -1,7 +1,8 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class IOStreams {
     public static void main(String[] args) {
@@ -41,6 +42,64 @@ public class IOStreams {
         -PrintWriter - Writes formatted representations of java objects to character stream
          */
 
+        //Using IO Streams
+        Path src = Path.of("sharks.log");
+        Path dest = Path.of("sharkscopy.log");
+        try {
+            readLazily(src);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    static void copyStream(InputStream in, OutputStream out) throws IOException{
+        int b;
+        while ((b = in.read()) != -1) out.write(b);
+    }
+    static void copyStream(Reader in, Writer out) throws IOException{
+        int b;
+        while ((b = in.read()) != -1) out.write(b);
+    }
+    static void copyStream2(InputStream in, OutputStream out) throws IOException{
+        int batchSize = 1024;
+        var buffer = new byte[batchSize];
+        int lengthRead;
+        while ((lengthRead = in.read(buffer,0,batchSize)) > 0){
+            out.write(buffer,0,lengthRead);
+            out.flush();
+        }
+    }
+    static void copyTextFile(File src, File dest) throws IOException{
+        try (BufferedReader reader = new BufferedReader(new FileReader(src))) {
+            var writer = new PrintWriter(new FileWriter(dest));
+            String line = null;
+            while ((line = reader.readLine()) != null){
+                writer.write(line);
+                writer.println();
+            }
+        }
+    }
+
+    //Enhancing with Files class (NIO2 API) - Much better!
+    static void copyPathAsString(Path input, Path output) throws IOException{
+        String string = Files.readString(input);
+        Files.writeString(output, string);
+    }
+    static void copyPathAsBytes(Path input, Path output) throws IOException{
+        byte[] bytes = Files.readAllBytes(input);
+        Files.write(output, bytes);
+    }
+    static void copyPathAsLines(Path input, Path output) throws IOException{
+        List<String> lines = Files.readAllLines(input);
+        Files.write(output,lines);
+    }
+    static void readLazily(Path path) throws IOException{
+        try (var s = Files.lines(path)) {
+            s.filter(f->f.startsWith("WARN:"))
+                .map(f->f.substring(5))
+                .forEach(System.out::println);
+        }
+    }
+
 }
